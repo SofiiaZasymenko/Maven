b43169efaeabf254e0c4df67129bc84fc1a9755f
SofiiaZasymenko <87094575+SofiiaZasymenko@users.noreply.github.com>
2023-05-04 00:04:31 +0300

    make user properties final

--- src/main/java/org/example/User.java
+++ src/main/java/org/example/User.java
@@ -1,11 +1,19 @@
 package org.example;
 
 public class User {
-    private String name;
-    private String lastName;
+    private final String name;
+    private final String lastName;
 
     public User(String name, String lastName) {
         this.name = name;
         this.lastName = lastName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLastName() {
+        return lastName;
     }
 }
